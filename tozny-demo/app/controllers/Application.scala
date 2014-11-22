e219c01fb27c1a079d90049465d922070e3b4425
Ryan Wright <rrwright@gmail.com>
2014-11-21 18:18:47 -0800

    minimal Play app

---
+++ tozny-demo/app/controllers/Application.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Tozny Title", "<h1>This is Tozny</h1>"))
+  }
+
+}
--- tozny-demo/build.sbt
+++ tozny-demo/build.sbt
@@ -2,5 +2,7 @@ name := "tozny-demo"
 
 version := "1.0"
 
+scalaVersion := "2.11.4"
+
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
