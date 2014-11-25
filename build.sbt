976d731b9dacb3b9b2628c9a82a230bfc919c89e
Kirk Peterson <necrobious@gmail.com>
2014-11-25 11:57:45 -0800

    adding basic template for handoof

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "sdk-scala"
+
+version := "1.0"
+
+scalaVersion := "2.11.4"
+
+//resolvers += "Typesafe repository" at "https://repo.typesafe.com/typesafe/releases/"
+
+//libraryDependencies ++= Seq(
+//  "com.typesafe.play" %% "play-ws" % "2.3.6"
+//)
+
---
+++ src/main/scala/com/tozny/RealmClient.scala
@@ -0,0 +1,20 @@
+package com.tozny
+
+/**
+ * Created by kirk on 11/24/14.
+ */
+
+import scala.concurrent.{Await, Future, ExecutionContext}
+
+import scala.concurrent.duration._
+
+object RealmClient  {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
+
+
+
