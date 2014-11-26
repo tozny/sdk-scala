3d47ff7e2a8280c31b6fb3f9ce4692801ee685c8
Kirk Peterson <necrobious@gmail.com>
2014-11-26 01:48:26 -0800

    Example Works

--- tozny-demo/app/controllers/Tozny.scala
+++ tozny-demo/app/controllers/Tozny.scala
@@ -3,10 +3,12 @@ package controllers
 import org.apache.commons.codec.binary.Base64
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.libs.json._
-import play.api.libs.json.DefaultReads.JsValueReads
+import play.api.libs.json.DefaultReads
 
-object Tozny extends Controller {
+object Tozny extends Controller with DefaultReads {
   case class ToznyLoginAttempt ( tozny_signed_data:String, tozny_signature:String)
   val form = Form(
     mapping(
@@ -19,17 +21,17 @@ object Tozny extends Controller {
     form.bindFromRequest.fold(
       invalid_attempt => {
         // binding failure, you retrieve the form containing errors:
-        BadRequest(views.html.index("Invalid attempt", "Error with form"))
+        BadRequest(views.html.index("Invalid attempt", "Error with form", ""))
       },
       valid_attempt => {
         // TODO
         val realm = new com.tozny.Realm("sid_54738a875dd7b", "dc3d1f7c855a570ec24aa768fe4f02f00eda0b3387adace9462b10639448e684", "https://api.tozny.com/index.php")
 
-        realm.verifyLogin(valid_attempt.tozny_signed_data, valid_attempt.tozny_signature) match {
+        realm.verifyLogin[JsValue](valid_attempt.tozny_signed_data, valid_attempt.tozny_signature) match {
           case Left(error) =>
-            BadRequest(views.html.index("Invalid signature", "Error with your signature:  "+error+ " "+(Json.parse(new String(new Base64(true).decode(valid_attempt.tozny_signed_data)))).getClass().getName))
-          case Right(json_response) =>
-            Ok("ok, I recived POST data. That's all...: tozny_signed_data: " + valid_attempt.tozny_signed_data + "; tozny_signature: " +valid_attempt.tozny_signature);
+            BadRequest(views.html.index("Invalid signature", "Error with your signature:  "+error, ""))
+          case Right(json_response:JsValue) =>
+            Ok("ok, I recived POST data. That's all...: "+json_response.toString());
         }
       }
     )
